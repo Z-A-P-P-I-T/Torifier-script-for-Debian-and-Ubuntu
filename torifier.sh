bffed7f83ebe0cb2aabfba594173bd41f405c584
rock-dotcom <79565366+rock-dotcom@users.noreply.github.com>
2023-01-10 10:37:29 +0100

    Updated torifier.sh
    Instead of creating a config.txt file I added the hashed password directly to the torrc configuration file using the printf command which prevent writing to file system and creating a file.
    I added a check for the current user shell before modifying the bashrc or zshrc file so that the shell persistancy step can be performed correctly on whichever shell the user is currently running.
    To check the IP, I used a service https://api.ipify.org for checking IP instead of command line tools like dig, host or nslookup
    I have also removed unnecessary sleep time and general clean up of the script.
    Change in shell persistancy option, added both torsocks on and source torsocks on to make sure it works for both zsh and bash shell
    The last step is to remove unnecessary steps such as deletion of the config.txt file and modification of the zhsrc file
    Please let me know if you have any questions on the changes.

--- torifier.sh
+++ torifier.sh
@@ -11,61 +11,73 @@ echo "
 sleep 0.5
 echo "                                                        
 Author: 	rock-dotcom (zappit)
-Creation Date:  20/3-21 - modified 28/6-22
+Creation Date:  20/3-21 - modified 10/1-23
 Description: 	Torproxy for Ubuntu and Debian Distros bash and zsh shell compatible. 
 "
 
+#update environment credentials to avoid asking for credentials everytime
+sudo -v
+
 sleep 2
 
+#Update system package
 sudo apt update && sudo apt upgrade -f -y
 
 sleep 0.5
 
 clear
 
+#Remove unneeded packages
 sudo apt autoremove -f -y
 
 sleep 0.5
 
 clear
+#install tor package
 sudo apt install tor -f -y
 
 sleep 0.2
 
 clear
 
+echo "Please set a password to protect your TOR connection:"
+read -p "Enter password: " NEWPASS
 
-echo "Please set a password to protect your TOR connection:
-"
- read -p "Enter password: " NEWPASS
+sleep 1
 
-	sleep 1
-	#creating config.txt
-	sudo echo > config.txt
-	sleep 1
+#create config.txt file
+sudo echo > config.txt
 
- sed -i -e"s/^pass=.*/pass=$NEWPASS/" config.txt
+sleep 1
+
+#update password in config.txt
+sed -i -e"s/^pass=.*/pass=$NEWPASS/" config.txt
 
 sleep 0.5
 
+#hash the password
 torpass=$(tor --hash-password '$NEWPASS')
 
 sleep 2
 
 clear
 
+#append hashed password to torrc file
 printf "HashedControlPassword $torpass\nControlPort 9051\n" | sudo tee -a /etc/tor/torrc
 
-echo "
-Your connection is now secured with a password 
-"
+echo "Your connection is now secured with a password"
+
 sleep 2
 
 clear
 
-echo "Restarting TOR to apply changes
-"
-sudo systemctl restart tor
+echo "Restarting TOR to apply changes"
+# Restarting tor service if it is running
+if  systemctl is-active --quiet tor; then
+    sudo systemctl restart tor
+else 
+    echo "tor service is not running, please check and start it"
+fi
 
 sleep 1
 
@@ -79,6 +91,7 @@ Do you want to turn torsocks on permanently
 for all new shell sessions and after reboot? 
 --------------------------------------------------------------
 "
+
 sleep 0.5
 
 while true; do
@@ -86,69 +99,57 @@ while true; do
     case $yn in
         [Yy]* ) 
         
-        echo "
-Turn's torsocks on permanently
-		"
-sleep 2
+        echo "Turning torsocks on permanently"
+        sleep 2
 
-#Both medthod for Enabeling/Disabeling added
-# ->
-torsocks off
-source torsocks off
+        #Both medthod for Enabling/Disabling added
+        # ->
+        torsocks off
+        source torsocks off
 
-sleep 2
+        sleep 2
 
-echo  "
- Modifing ~/.bachrc - Please Wait"
- 
-echo ". torsocks on" >> ~/.bashrc
+        echo  "Modifying ~/.bashrc - Please Wait"
+        echo ". torsocks on" >> ~/.bashrc
 
-sleep 2
+        sleep 2
 
-echo "
- Modifing ~/.zhsrc - Please Wait
-"
+        echo "Modifying ~/.zshrc - Please Wait"
+        echo ". torsocks on" >> ~/.zshrc
 
-echo ". torsocks on" >> ~/.zhsrc
+        sleep 2
 
-sleep 2
+        echo "Restarting TOR - Please Wait"
+        sudo systemctl restart tor
 
-echo " Restarting TOR - Please Wait
-"
-sudo systemctl restart tor
+        sleep 2
 
-sleep 2
+        echo "Setting torsocks on - Please Wait"
+        sleep 2
 
-echo " Setting torsock on - Please Wait
-"
-sleep 2
+        #Both method for Enabling/Disabling added
+        # ->
+        torsocks on
+        source torsocks on
 
-#Both medthod for Enabeling/Disabeling added
-# ->
-torsocks on
-source torsocks on
+        sleep 2
 
-sleep 2
+        clear
 
-clear
+        break;;
 
-break;;
-        
-        
         [Nn]* ) 
-clear            
-echo "********** .You choose to set torsocks maually. ************** "
-sleep 1
-           break ;;
+        clear            
+        echo "You chose to set torsocks manually."
+        sleep 1
+        break ;;
 
-
-#        * ) echo "Please answer yes or no.";;
+        * ) echo "Please answer yes or no.";;
     esac
-    
 done
 
 echo "--------------------------------------------------------------
-Please use the fallowing commands to activate/deactivate TOR:
+Please use the following commands to activate/deactivate TOR:
 
 $ source torsocks on
 (( Tor mode activated )) 
@@ -156,13 +157,15 @@ $ source torsocks on
 $ source torsocks off
 (( Tor mode deactivated ))
 "
+#Checking IP outide the tor network
 echo "Please wait, checking your ip outside the TOR network"
-	wget -qO - https://api.ipify.org; echo
-		sleep 0.25
+wget -qO - https://api.ipify.org; echo
+sleep 0.25
 
+#Checking IP inside the tor network
 echo "Please wait, checking your TOR ip."
-	torsocks wget -qO - https://api.ipify.org; echo 		
-		sleep 0.25
+torsocks wget -qO - https://api.ipify.org; echo 		
+sleep 0.25
 		
 echo "             Thank you for using this script.                 "
 echo "                    Have a great day!                         "
